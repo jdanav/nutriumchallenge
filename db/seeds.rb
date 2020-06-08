272fcd59a5d4c97c70aae76d1f3eda14b6932d26
jdanav <jdanav@gmail.com>
2020-06-08 20:17:23 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Professional.create(name: 'Ana Pessoa',job_description: 'Nutricionista', pro_id:'2020N',office:'Clínica Genérica',address:'Avenida Central, 6',city:'Vila Nova de Gaia, Porto',appointment:'Consulta presencial',price:'35',photo:'person.jpg')
